966763023557c201be6ffa2aec7ec7c83d2986ef
kesiahp18 <kesiahp18@gmail.com>
2020-12-27 17:58:46 -0500

    fixed template

--- src/md-template.js
+++ src/md-template.js
@@ -1,32 +1,31 @@
 module.exports = templateData => {
     const { projectName, ...data } = templateData;
     return `
-    # ${projectName}
-    ## Description
-    ${data.description}
-    ## Table of Contents
-    * [Installation](github.com/${data.username}/${projectName}#installation)
-    * [Usage](github.com/${data.username}/${projectName}#usage)
-    * [License](github.com/${data.username}/${projectName}#license)
-    * [Contributing](github.com/${data.username}/${projectName}#contributing)
-    * [Tests](github.com/${data.username}/${projectName}#tests)
-    * [Questions](github.com/${data.username}/${projectName}#questions)
+# ${projectName}
+## Description
+${data.description}
+## Table of Contents
+* [Installation](github.com/${data.username}/${projectName}#installation)
+* [Usage](github.com/${data.username}/${projectName}#usage)
+* [License](github.com/${data.username}/${projectName}#license)
+* [Contributing](github.com/${data.username}/${projectName}#contributing)
+* [Tests](github.com/${data.username}/${projectName}#tests)
+* [Questions](github.com/${data.username}/${projectName}#questions)
     
-    ## Installation 
-    ${data.installation}
-    ## Usage
-    ${data.usage}
+## Installation 
+${data.installation}
+## Usage
+${data.usage}
 
 
-    ## License
-    ${data.license}
+## License
+${data.license}
 
-    ## Contributing
+## Contributing
 
-    ## Tests
+## Tests
 
-    ## Questions?
-    If you have any questions send an email to ${data.email} to have your questions answered.
-
-    `;
+## Questions?
+If you have any questions send an email to ${data.email} to have your questions answered.
+`;
 };
